08517c977465d6572fc38abd71f3f9cac27927b4
JoaoVNicacio <joaonicks99@gmail.com>
2022-08-17 17:57:48 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# e-Core Devs'cool: Case Técnico
+João Victor Nicácio
+
+### PT-BR:
+## A solução:
+### A solução se trata de uma aplicação JavaScript no console atendendo as seguintes funcionalidades:
+
+- A funcionalidade do programa é o armazenamento de dados cadastrais de pessoas. Devem constar os seguintes dados no cadastro: Nome e idade.
+
+- Deve ser possível cadastrar / adicionar uma nova pessoa.
+
+- Deve ser possível exibir uma lista de pessoas cadastradas, ordenada por idade (menor para maior) e nome (alfabética crescente).
+
+- Deve ser possível categorizar as pessoas cadastradas por idade, de acordo com os critérios: 0 a 12 anos => Criança, 12 até 19 anos => Adolescente, 20 até 65 => Adulto, 65 ou mais => Idoso.
+
+> Essa solução pode ser testada com o código no index sendo copiado e executado no console do navegador ou se preferir...
+>
+>> [Teste rapidamente pelo repl.it](https://replit.com/join/tzvtjaxbyx-joaovictor75)
+>> Ou copie o link: https://replit.com/join/tzvtjaxbyx-joaovictor75
+
+
+### EN:
+## The solution:
+### the solution is a JavaScript application on the console attending these features:
+
+- The core of the software is the hosting of registration data of people. The registration must have the following: Name and age.
+
+- It must be possible to add a new person to the list.
+
+- It must be possible to show a registered people list, sorted by age (Increasing order) and name (Increasing alphabetical order).
+
+- It must be possible to categorize people by age, Following the criteria: 0 up to 12 years => Child, 12 up to 19 years => Teenager, 20 up to 65 => Adult, 65 or more => Elder.
+
+> This solution can be tested by pasting and executing the index.js code on the browser console or if you prefer...
+>
+>> [Test it on repl.it](https://replit.com/join/tzvtjaxbyx-joaovictor75)
+>> Or copy the link link: https://replit.com/join/tzvtjaxbyx-joaovictor75
+
+
+
---
+++ index.js
@@ -0,0 +1,87 @@
+// Control variables:
+let validation = true;
+let i = 0;
+// List of people:
+let peopleList = [];
+let sortedPeopleList = [];
+
+// Creating the person class:
+class Person {
+    constructor(name, age, ageRange) {
+        this.name = name;
+        this.age = age;
+
+        // Define the age range:
+        if (age >= 0 && age <= 12) {
+            ageRange = `Child`;
+
+        } else if (age > 12 && age <= 19) {
+            ageRange = `Teenager`;
+
+        } else if (age > 19 && age <= 65) {
+            ageRange = `Adult`;
+
+        } else {
+            ageRange = `Elder`;
+
+        }
+
+        this.ageRange = ageRange;
+    }
+}
+
+// Function to add people with the inputs
+function addPerson() {
+    // Getting the inputs:
+    let nameInput = prompt(`What's the name of the new person? `);
+    let ageInput = parseInt(prompt(`What's the age of the new person? `));
+
+    // Re-enter the age if it´s less than 0, not a number, null or undefined:
+    while (ageInput < 0) {
+        ageInput = prompt(`${ageInput} is not a valid age, please enter a valid one. `);
+    }
+
+    // Creating and returning the object:
+    let newPersonInput = new Person(nameInput, ageInput);
+    return newPersonInput;
+}
+
+// Function for sorting the list in a crescent order alphabetically and numerically:
+function sortList(list) {
+    let sortedList = list.sort((a, b) => {
+        return a.name.localeCompare(b.name) || a.age - b.age;
+    })
+
+    return sortedList;
+}
+
+console.log(`Welcome to the e-Core test list! let's add some people? what do you think? \n`);
+
+// while true for validation and adding people to the list
+while (validation === true) {
+    let responseToAddNewUser = prompt(`Do you want to add someone? (y) to yes / (n) to no. `);
+    console.clear();
+
+    if (responseToAddNewUser.toLowerCase() === 'y') {
+        // Adding the person to the list: 
+        peopleList[i] = addPerson();
+
+        i++
+        console.clear();
+
+    } else if (responseToAddNewUser.toLowerCase() === 'n') {
+        console.log(`Ok! we're going to organize and show you the list! \n`);
+        validation = false;
+
+    } else {
+        console.log(`Oops, we think you added an invalid input, could you enter it again? `);
+
+    }
+
+}
+
+// Creating the sorted list and showing it:
+sortedPeopleList = sortList(peopleList);
+sortedPeopleList.length === 0
+    ? console.log(`Your list is empty, so we don't have people to show :( `)
+    : console.table(sortedPeopleList);
