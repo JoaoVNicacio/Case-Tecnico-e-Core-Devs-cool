4d1c34e6244b6837369697c238b8c2d194b1c8e1
João Victor Nicacio <74374178+JoaoVNicacio@users.noreply.github.com>
2022-09-06 13:26:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 // Control variables:
 let validation = true;
-let i = 0;
 // List of people:
 let peopleList = [];
 let sortedPeopleList = [];
 
+
 // Creating the person class:
 class Person {
+  
     constructor(name, age, ageRange) {
+      
         this.name = name;
         this.age = age;
 
@@ -30,15 +32,28 @@ class Person {
     }
 }
 
-// Function to add people with the inputs
-function addPerson() {
-    // Getting the inputs:
+
+// Function to add people with the inputs:
+const addPerson = () => {
+  
+    // Getting the name input:
     let nameInput = prompt(`What's the name of the new person? `);
+  
+  // Re-entering the name if it has nothing in it:
+  while (nameInput.split("").length < 1){
+    nameInput = prompt(`This is not a valid name, please enter a valid one. `);
+  }
+
+  // Getting the age input:
     let ageInput = parseInt(prompt(`What's the age of the new person? `));
 
-    // Re-enter the age if it´s less than 0:
+    // Re-enter the age if it´s less than 0 or Not a number:
     while (ageInput < 0) {
-        ageInput = prompt(`${ageInput} is not a valid age, please enter a valid one. `);
+        ageInput = parseInt(prompt(`${ageInput} is not a valid age, please enter a valid one. `));
+    }
+  
+   while (isNaN(ageInput)) {
+        ageInput = parseInt(prompt(`${ageInput} is not a valid age, please enter a valid one. `));
     }
 
     // Creating and returning the object:
@@ -46,8 +61,9 @@ function addPerson() {
     return newPersonInput;
 }
 
+
 // Function for sorting the list in a crescent order alphabetically and numerically:
-function sortList(list) {
+const sortList = (list) => {
     let sortedList = list.sort((a, b) => {
         return a.name.localeCompare(b.name) || a.age - b.age;
     })
@@ -55,18 +71,20 @@ function sortList(list) {
     return sortedList;
 }
 
+
+// Begining:
 console.log(`Welcome to the e-Core test list! let's add some people? what do you think? \n`);
 
-// while true for validation and adding people to the list
+// while true for validation and adding people to the list:
 while (validation === true) {
     let responseToAddNewUser = prompt(`Do you want to add someone? (y) to yes / (n) to no. `);
     console.clear();
 
     if (responseToAddNewUser.toLowerCase() === 'y') {
         // Adding the person to the list: 
-        peopleList[i] = addPerson();
+        peopleList.push(addPerson());
 
-        i++
+       
         console.clear();
 
     } else if (responseToAddNewUser.toLowerCase() === 'n') {
@@ -77,7 +95,6 @@ while (validation === true) {
         console.log(`Oops, we think you added an invalid input, could you enter it again? `);
 
     }
-
 }
 
 // Creating the sorted list and showing it:
